ccae851e4fb195e20cd576c630f13e4fb3aa1b03
inspirrrada <sem4ykk@gmail.com>
2023-01-20 01:39:55 +0200

    hw_6: reformate code

--- hw_7_dictionary/src/main/java/ua/com/alevel/dictionary/DictionaryDemonstrateMethods.java
+++ hw_7_dictionary/src/main/java/ua/com/alevel/dictionary/DictionaryDemonstrateMethods.java
@@ -27,18 +27,18 @@ public class DictionaryDemonstrateMethods {
 //        cutAndClearDemonstration();
     }
 
-    public void
-
-    int size()
-    boolean isEmpty()
-    boolean containsKey(K key)
-    boolean containsValue(V value)
-    V get(K key)
-    boolean put(K key, V value)
-    boolean remove(K key)
-    boolean putAll(Dictionary<K, V> dictionary)
-    boolean clear()
-    Set<K> keySet()
-    Collection<V> values()
+//    public void
+//
+//    int size()
+//    boolean isEmpty()
+//    boolean containsKey(K key)
+//    boolean containsValue(V value)
+//    V get(K key)
+//    boolean put(K key, V value)
+//    boolean remove(K key)
+//    boolean putAll(Dictionary<K, V> dictionary)
+//    boolean clear()
+//    Set<K> keySet()
+//    Collection<V> values()
 
 }

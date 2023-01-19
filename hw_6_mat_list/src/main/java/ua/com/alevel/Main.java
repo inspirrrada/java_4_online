af5dd93614d15b2cb282e5ec1de9f2e3b903b5b0
inspirrrada <sem4ykk@gmail.com>
2023-01-20 01:25:18 +0200

    hw_6: reformate code

--- hw_6_mat_list/src/main/java/ua/com/alevel/Main.java
+++ hw_6_mat_list/src/main/java/ua/com/alevel/Main.java
@@ -5,7 +5,6 @@ import ua.com.alevel.matlist.MatListDemonstrateMethods;
 
 public class Main {
     public static void main(String[] args) {
-
-     new MatListDemonstrateMethods().start();
+        new MatListDemonstrateMethods().start();
     }
 }

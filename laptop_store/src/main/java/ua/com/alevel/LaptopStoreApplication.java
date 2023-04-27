6dc5123d1b5ada95ac3500c659612536bc1cf7e1
inspirrrada <sem4ykk@gmail.com>
2023-04-27 14:50:03 +0300

    laptop_store: add spring configuration

--- laptop_store/src/main/java/ua/com/alevel/LaptopStoreApplication.java
+++ laptop_store/src/main/java/ua/com/alevel/LaptopStoreApplication.java
@@ -1,7 +1,11 @@
 package ua.com.alevel;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class LaptopStoreApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world from laptop_store!");
+        SpringApplication.run(LaptopStoreApplication.class, args);
     }
 }
